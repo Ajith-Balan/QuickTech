a40bf4747832af27a8e5d47cd64c20d3306287b9
AJITH B <139620991+Ajith-Balan@users.noreply.github.com>
2024-11-18 20:33:04 +0530

    Update Premium.jsx

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -99,7 +99,7 @@ const CategoryPremium = () => {
                   <div className="p-4">
                     <h2 className="text-2xl font-semibold text-gray-800">{product.name}</h2>
 
-                      <a     href={"https://ajith-balan.github.io/woody/"}
+                      <a     href={product.livelink}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="flex   text-blue-800">
